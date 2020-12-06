f80c9d71b4429bb9b7a993d682cf5b1a447507a0
berperez2003 <berperez2003@yahoo.com.ar>
2020-12-05 22:54:57 -0500

    inclui los intergrantes del equipo con vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,59 @@
 <template>
   <div id="app">
+    <div class="container-fluid ">
+    <div class="row justify-content-center mb-5 " >
+      <div class="col mt-5" v-for="(item,index) of team" :key="index" >
+        <team-card :member="item"></team-card>
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import TeamCard from './components/TeamCard.vue'
 export default {
   name: 'App',
+    components:{
+      TeamCard,
+  } ,
+  data(){
+    return{
+      team:[
+            {   codigo: 1,
+                nombre: 'Diego Mauricio Sanchez',
+                descripcion: 'Ingeniero de sistemas de la Universidad Distrital, de 41 años de edad. Aficionado al ajedrez y a los RPG. Papá de Juandi y de dos gatos.',
+                rol:'Scrum Master',
+                image: require('@/assets/foto_4.jpg') 
+            },
+            {   codigo: 2,
+                nombre: 'Juan David Avalos',
+                descripcion: 'Ingeniero de Petróleos de 29 años de edad. Reparte su afición entre el fútbol y los videojuegos. Felizmente casado.',
+                rol:'Development Team',
+                image: require('@/assets/foto_1.jpg') 
+            },
+            {   codigo: 3,
+                nombre: 'Jose Bernardo Perez',
+                descripcion: 'Administrador de un taller de taxímetros, con 45 años de edad, su mayor afición es saber cómo funcionan las cosas, tiene una hija una gata y muchos peces.',
+                rol:'Development Team',
+                image: require('@/assets/foto_2.jpg') 
+            },
+            {   codigo: 4,
+                nombre: 'Juan Pablo Bermúdez',
+                descripcion: 'Economista de la Universidad Nacional. Me gusta  Radiohead, Steven Wilson y  el Techno. Suelo hacer ejercicio o trotar cuando el clima es favorable y a veces juego lol. Tengo dos gatos.',
+                rol:'Development Team',
+                image: require('@/assets/foto_3.jpg') 
+            },
+            {   codigo: 5,
+                nombre: 'Joan Esneider Duque',
+                descripcion: 'Estudiante de Ingeniería Mecánica de la Universidad de Antioquia actualmente en noveno semestre, el poco tiempo libre que me queda lo comparto con mi hija.',
+                rol:'Development Team',
+                image: require('@/assets/foto_5.jpg') 
+            },
+        ]
+    }    
+  }
+
 }
 </script>
 
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,13 +1,23 @@
-<template>
-    <div>
-
-    </div>
+<template>            
+    <div class="card  text-red bg-warning h-100">
+        <div class="d-flex justify-content-center p-2">
+            <img class= "card-img-top" :src="member.image" alt= "foto del integrante">
+        </div>
+        <div class="card-body text-center">
+            <h3 class="card-title">{{member.nombre}}</h3>
+            <p class="card-text">{{member.descripcion}}</p>            
+            <h4 class="card-title">{{member.rol}}</h4>            
+            <p class="card-text">Codigo : {{member.codigo}}</p>
+        </div>        
+    </div>   
 </template>
 
 <script>
     export default {
         name: "TeamCard",
-        props: ['member']
+        props: {
+            'member': Object
+        }
     }
 </script>
 
